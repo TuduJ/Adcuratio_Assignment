4bce76051ff6397cfd94c80bef5f0ab4b0a8fbe3
TuduJ <jitendratd@gmail.com>
2021-01-17 11:07:35 +0000

    seed data updata

--- seed.js
+++ seed.js
@@ -10,13 +10,30 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUn
 })
 
 
-const d = new Data({
-	id: "3",
-	name: "test3",
-	age: "13",
-	gender: "male",
-	email: "test3@gmail.com",
-	phoneNo: "9393939393"
+const d = new Data(
+	{
+		id: "1",
+		name: "test",
+		age: "11",
+		gender: "male",
+		email: "test1@gmail.com",
+		phoneNo: "9191919191"
+	},
+	{
+		id: "2",
+		name: "test2",
+		age: "12",
+		gender: "male",
+		email: "test2@gmail.com",
+		phoneNo: "9292929292"
+	},
+	{
+		id: "3",
+		name: "test3",
+		age: "13",
+		gender: "male",
+		email: "test3@gmail.com",
+		phoneNo: "9393939393"
 })
 
 d.save().then(p => {
